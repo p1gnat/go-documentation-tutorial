6f8fa507e8999b244e3906bf2dd89f3dddbedc74
p1gnat <hellwowo1@gmail.com>
2025-02-20 15:19:03 +0500

    random greetings/ my  package

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,25 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello (name string) string {
-	string := fmt.Sprintf("Hi %v, welcome!", name)
-	return string
+func Hello (name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	string := fmt.Sprintf(randomFormat(), name)
+	return string, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi %v, welcome!",
+		"Great to see you %v",
+		"Hail, %v, Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
